1d8770dcfdfc1b954d769d02fec12664e4b81e67
akihiro nagashima <akihiro.nagashima@gmail.com>
2017-10-28 19:08:27 +0900

    微調整

--- Assets/CamController.cs
+++ Assets/CamController.cs
@@ -22,8 +22,8 @@ public class CamController : MonoBehaviour
 		if (Input.GetKey(KeyCode.Space))
 		{
 			// 中心位置, 軸, 度数
-			transform.RotateAround(new Vector3(2.5f, 0, 2.5f), new Vector3(0, 1, 0), -3.5f);
-			Camera.main.DOFieldOfView(1, 3f);
+			transform.RotateAround(new Vector3(2.5f, 0, 2.5f), new Vector3(0, 1, 0), -3f);
+			Camera.main.DOFieldOfView(1, 2f);
 		}
     }
 }
