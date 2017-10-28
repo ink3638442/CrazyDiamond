70f29cca819044a3627786be8ab79cd22de0e03c
akihiro nagashima <akihiro.nagashima@gmail.com>
2017-10-28 18:50:27 +0900

    カメラコントローラー

---
+++ Assets/CamController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CamController : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Camera.main.DOFieldOfView(100, 2);
+
+		// 中心位置, 軸, 度数
+		transform.RotateAround(new Vector3(2.5f, 0, 2.5f), new Vector3(0, 1, 0), 0.5f);
+
+		if (Input.GetKey(KeyCode.Space))
+		{
+			// 中心位置, 軸, 度数
+			transform.RotateAround(new Vector3(2.5f, 0, 2.5f), new Vector3(0, 1, 0), -3.5f);
+			Camera.main.DOFieldOfView(1, 3f);
+		}
+    }
+}
