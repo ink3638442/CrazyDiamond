426f02b80be139aaf0b54fc9edbd42112a4f03a7
akihiro nagashima <akihiro.nagashima@gmail.com>
2017-10-28 13:43:07 +0900

    集合キューブの作成

---
+++ Assets/CubeManeger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class CubeManeger : MonoBehaviour
+{
+	const int CUBE_NUMS = 5;
+
+    [SerializeField]
+	GameObject cubePrefab;
+
+    List<GameObject> cubeList = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int x = 0; x < CUBE_NUMS; x++)
+        {
+            for (int y = 0; y < CUBE_NUMS; y++)
+            {
+                for (int z = 0; z < CUBE_NUMS; z++)
+                {
+                    GameObject go = Instantiate(cubePrefab) as GameObject;
+                    go.transform.position = new Vector3(x, y, z);
+
+                    cubeList.Add(go);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
